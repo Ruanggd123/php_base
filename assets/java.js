96ebce44c27e5c1668ca32f88b2da738dc6ca4a4
Ruan Gomes <ruangmes159@gmail.com>
2023-02-28 10:54:07 -0300

    adiconado joguinho para teste e treinamento

--- assets/java.js
+++ assets/java.js
@@ -4,15 +4,22 @@
 */
 /*document.write("Alguma coisa");
 document.write("<img src='http://www.google.com.br/google.jpg'/>");
-*/
+
 function fuc(nome, idade){
     var novo = document.getElementById("area2");
     var texto = prompt("Qual o seu sobrenome ?");
     novo.innerHTMl= nome+" "+texto+" tem "+idade+" anos";
 }
 function adicionarI(){
-    var ing = documenht.getElementById("ingrediente");
-    var listahtml = document.getElementById("lista").innerHTLM;
+    var ing = document.getElementById("ingrediente").value;
+    var listahtml = document.getElementById("lista").innerHTMl;
     listahtml = listahtml +"<li>" + ing + "</li>";
-    document.getElementById("lista").innerHTML=listahtml;
+    document.getElementById("lista").innerHTMl=listahtml;
 }
+function valorsoma(){
+    var num1 = parseInt(document.getElementById("num1").value);
+    var num2 = parseInt(document.getElementById("num2").value);
+    
+    var soma = num1 + num2;
+    document.getElementById("valor").innerHTML= soma;
+}*/
